fe3ac9c2fe1d570a0b11188ad78dec7d848f36ca
Declan Naughton <declann1990@gmail.com>
2026-02-17 23:02:43 +0000

    run formatter (speculative)

--- src/cul/basicterm.cul.js
+++ src/cul/basicterm.cul.js
@@ -21,20 +21,17 @@
 // An old check that tied back for 10k of lifelib simulated policies :
 // https://x.com/calculang/status/1811044584207560929
 
-
-import { premium_rate, mort_rate, zero_spot } from './basicterm-tables.cul.js'
-
+import { premium_rate, mort_rate, zero_spot } from "./basicterm-tables.cul.js";
 
 // time in months, >=0 is future, <0 is past
-export const t = () => t_in ?? 0
+export const t = () => t_in ?? 0;
 
 // 'model point' inputs
-export const age_at_entry = () => age_at_entry_in ?? 20
-export const sex = () => sex_in ?? 'M'
-export const policy_term = () => policy_term_in ?? 10
-export const policy_count = () => policy_count_in ?? 1
-export const sum_assured = () => sum_assured_in ?? 100000
-
+export const age_at_entry = () => age_at_entry_in ?? 20;
+export const sex = () => sex_in ?? "M";
+export const policy_term = () => policy_term_in ?? 10;
+export const policy_count = () => policy_count_in ?? 1;
+export const sum_assured = () => sum_assured_in ?? 100000;
 
 // duration_mth_0_in is an input: the duration "now" since policy started (or 0 for a New Business projection)
 // but duration_mth() is the duration at t since policy started
@@ -46,14 +43,14 @@ export const duration_mth_0 = () => duration_mth_0_in ?? 0;
 // duration since policy started, in years:
 export const duration = () => Math.floor(duration_mth() / 12);
 
-export const age = () => age_at_entry() + duration()
+export const age = () => age_at_entry() + duration();
 
 // AMOUNT TO PAY ON CLAIM is determed by claim_pp formula
 
 // It's used to project claims cashflows And in the claims projection to calculate premiums (see playground.cul.js `premium_rate_per_mille`)
 
 // for level term assurance, we use a flat sum assured:
-export const claim_pp = () => sum_assured()
+export const claim_pp = () => sum_assured();
 
 // for reducing term assurance, switch to this for a straight-line reduction:
 //export const claim_pp = () => sum_assured() * (1 - duration() / policy_term())
@@ -66,35 +63,35 @@ export const claim_pp = () => sum_assured()
 // This is an example of model composition: calculang models can use other models
 // and an every-day URL is an effective way to share and re-use models and logic (also workings)
 
-
-
 // mortality rates, using mort_rate() from basicterm-tables.cul.js
 
-export const mort_rate_select_index = () => Math.max(0, Math.min(5, duration())) // used to lookup mort_rate table
+export const mort_rate_select_index = () =>
+  Math.max(0, Math.min(5, duration())); // used to lookup mort_rate table
 
 // I can hoist this change to playground.cul.js, but it provides context for pols_if_at behaviour here
-export const zero_decrement_experience = () => zero_decrement_experience_in ?? true // todo consider without this?
+export const zero_decrement_experience = () =>
+  zero_decrement_experience_in ?? true; // todo consider without this?
 
 export const mort_rate_mth = () => {
   if (t() < 0 && zero_decrement_experience()) return 0;
-  return 1 - (1 - mort_rate()) ** (1 / 12)
-}
+  return 1 - (1 - mort_rate()) ** (1 / 12);
+};
 
 // lapse rates
 // tihs formula can get updated in the UI:
-export const lapse_rate = () => Math.max(0.1 - 0.02 * duration(), 0.02)
+export const lapse_rate = () => Math.max(0.1 - 0.02 * duration(), 0.02);
 
-export const lapse_rate_mth = () => { // factored from pols_lapse
+export const lapse_rate_mth = () => {
+  // factored from pols_lapse
   if (t() < 0 && zero_decrement_experience()) return 0;
-  return (1 - (1 - lapse_rate()) ** (1 / 12))
-}
-
+  return 1 - (1 - lapse_rate()) ** (1 / 12);
+};
 
 // policy survival/decrement projections
 
-export const timing = () => timing_in ?? 'BEF_NB'
+export const timing = () => timing_in ?? "BEF_NB";
 
-export const pols_if_init = () => policy_count()
+export const pols_if_init = () => policy_count();
 
 // BasicTerm_SE wants to project in force data at some date, into the future: it doesn't care about past cashflows.
 // In the Playground I want to show past cashflows.
@@ -103,73 +100,87 @@ export const pols_if_init = () => policy_count()
 // TODO paramaterise this behaviour incl. checks and illustrate pols_if_at timing and logic
 
 export const pols_if_at = () => {
-  if (is_active() == 0) return 0
-  if (timing() == 'BEF_MAT') {
+  if (is_active() == 0) return 0;
+  if (timing() == "BEF_MAT") {
     if (t() == 0 && pols_if_init() && 0 /* DN: disabling */)
       return pols_if_init();
-    return pols_if_at({ t_in: t() - 1, timing_in: 'BEF_DECR' }) - pols_lapse({ t_in: t() - 1 }) - pols_death({ t_in: t() - 1 })
+    return (
+      pols_if_at({ t_in: t() - 1, timing_in: "BEF_DECR" }) -
+      pols_lapse({ t_in: t() - 1 }) -
+      pols_death({ t_in: t() - 1 })
+    );
   }
-  if (timing() == 'BEF_NB') {
-    return pols_if_at({ timing_in: 'BEF_MAT' }) - pols_maturity()
+  if (timing() == "BEF_NB") {
+    return pols_if_at({ timing_in: "BEF_MAT" }) - pols_maturity();
   }
-  if (timing() == 'BEF_DECR') return pols_if_at({ timing_in: 'BEF_NB' }) + pols_new_biz()
-  return console.error('bad timing_in !')
-}
+  if (timing() == "BEF_DECR")
+    return pols_if_at({ timing_in: "BEF_NB" }) + pols_new_biz();
+  return console.error("bad timing_in !");
+};
 
-export const pols_lapse = () => is_active() ? (pols_if_at({ timing_in: 'BEF_DECR' }) - pols_death()) * lapse_rate_mth() : 0
-
+export const pols_lapse = () =>
+  is_active()
+    ? (pols_if_at({ timing_in: "BEF_DECR" }) - pols_death()) * lapse_rate_mth()
+    : 0;
 
 // refactor for stress and experience logic
 
-
 export const pols_maturity = () => {
   if (duration_mth() == policy_term() * 12)
-    return pols_if_at({ timing_in: 'BEF_MAT' })
-  else return 0
-}
+    return pols_if_at({ timing_in: "BEF_MAT" });
+  else return 0;
+};
 
 export const pols_new_biz = () => {
-  if (duration_mth() == 0)
-    return policy_count()
-  else return 0
-}
+  if (duration_mth() == 0) return policy_count();
+  else return 0;
+};
 
-export const pols_death = () => is_active() ? pols_if_at({ timing_in: 'BEF_DECR' }) * mort_rate_mth() : 0;
+export const pols_death = () =>
+  is_active() ? pols_if_at({ timing_in: "BEF_DECR" }) * mort_rate_mth() : 0;
 
 // monthly
-export const premium_pp = () => Math.round(sum_assured() / 1000 * premium_rate_per_mille() * 100) / 100 // round 2 decimal places
+export const premium_pp = () =>
+  Math.round((sum_assured() / 1000) * premium_rate_per_mille() * 100) / 100; // round 2 decimal places
 
+export const expense_acq = () => 300;
+export const expense_maint = () => 60;
 
-export const expense_acq = () => 300
-export const expense_maint = () => 60
+export const inflation_rate = () => 0.01;
+export const inflation_rate_mth = () => (1 + inflation_rate()) ** (1 / 12) - 1;
 
-export const inflation_rate = () => 0.01
-export const inflation_rate_mth = () => (1 + inflation_rate()) ** (1 / 12) - 1
-
-export const inflation_factor = () => (1 + inflation_rate()) ** (t() / 12)
+export const inflation_factor = () => (1 + inflation_rate()) ** (t() / 12);
 
 // cashflows:
 
-export const premiums = () => premium_pp() * pols_if_at({ timing_in: 'BEF_DECR' })
-export const claims = () => -claim_pp() * pols_death()
+export const premiums = () =>
+  premium_pp() * pols_if_at({ timing_in: "BEF_DECR" });
+export const claims = () => -claim_pp() * pols_death();
 
 // acquisition expenses are not affected by inflation (questionable but consistent with BasicTerm_S https://lifelib.io/_modules/basiclife/BasicTerm_S/Projection.html#expenses)
-export const expenses_acq = () => expense_acq() * pols_new_biz()
-export const expenses_maint = () => (expense_maint() / 12) * inflation_factor() * pols_if_at({ timing_in: 'BEF_DECR' }) // monthly maintenance expenses, inflated
-export const expenses = () => -(expenses_acq() + expenses_maint())
-export const commissions = () => (duration_mth() < commission_mths()) ? (-premiums() * commission_pc() / 100) : 0
+export const expenses_acq = () => expense_acq() * pols_new_biz();
+export const expenses_maint = () =>
+  (expense_maint() / 12) *
+  inflation_factor() *
+  pols_if_at({ timing_in: "BEF_DECR" }); // monthly maintenance expenses, inflated
+export const expenses = () => -(expenses_acq() + expenses_maint());
+export const commissions = () =>
+  duration_mth() < commission_mths()
+    ? (-premiums() * commission_pc()) / 100
+    : 0;
 
-export const commission_mths = () => 12
-export const commission_pc = () => 100
+export const commission_mths = () => 12;
+export const commission_pc = () => 100;
 
 // careful: manipulation in net_cf not captured in Playground outputs!
-export const net_cf = () => premiums() + claims() + expenses() + commissions()
+export const net_cf = () => premiums() + claims() + expenses() + commissions();
 
-
-export const proj_len = () => Math.max(12 * policy_term() - duration_mth({ t_in: 0 }) + 1, 0)
+export const proj_len = () =>
+  Math.max(12 * policy_term() - duration_mth({ t_in: 0 }) + 1, 0);
 
 // todo think re actuals (new)
-export const is_active = () => !(duration_mth() < 0 || duration_mth() > policy_term() * 12)
+export const is_active = () =>
+  !(duration_mth() < 0 || duration_mth() > policy_term() * 12);
 
 export const pols_if = () => pols_if_at(/*{ timing_in: 'BEF_MAT' }*/);
 // pols_if in lifelib BasicTerm_S uses BEF_MAT timing, but there pols_if isn't actually used.
@@ -180,26 +191,25 @@ export const pols_if = () => pols_if_at(/*{ timing_in: 'BEF_MAT' }*/);
 // Leaving timing free here: I specify the timing in premium_rate_per_mille formula in playground.cul.js
 
 export const net_premium_pp = () => {
-  return -pv_fut_claims({ t_in: 0, duration_mth_0_in: 0 }) / pv_fut_pols_if({ t_in: 0, duration_mth_0_in: 0 })
-}
+  return (
+    -pv_fut_claims({ t_in: 0, duration_mth_0_in: 0 }) /
+    pv_fut_pols_if({ t_in: 0, duration_mth_0_in: 0 })
+  );
+};
 
-export const premium_rate_per_mille = () =>
-  premium_rate() * 1000; // uses table from basicterm-tables.cul.js
-
-
+export const premium_rate_per_mille = () => premium_rate() * 1000; // uses table from basicterm-tables.cul.js
 
 // Discounting factor calculation
 // derived from zero_spot() from basicterm-tables.cul.js
 
 export const disc_rate_ann = () => {
   if (t() <= 0) return 0;
-  else return zero_spot({ zero_spot_year_in: Math.floor(t() / 12) })
-}
+  else return zero_spot({ zero_spot_year_in: Math.floor(t() / 12) });
+};
 
-export const disc_rate_mth = () => (1 + disc_rate_ann()) ** (1 / 12) - 1
+export const disc_rate_mth = () => (1 + disc_rate_ann()) ** (1 / 12) - 1;
 
-export const disc_factor = () => (1 + disc_rate_mth()) ** (-t())
-
+export const disc_factor = () => (1 + disc_rate_mth()) ** -t();
 
 // Present Value calculations
 // In 2 parts so that pv_x can conveniently visualise discounted cashflows over time
@@ -210,56 +220,54 @@ export const disc_factor = () => (1 + disc_rate_mth()) ** (-t())
 // It's unsightly but it can be separated from other modelling logic.
 
 // 1. pv_x is the present value is month t
-export const pv_claims = () => claims() * disc_factor()
-export const pv_premiums = () => premiums() * disc_factor()
-export const pv_expenses = () => expenses() * disc_factor()
-export const pv_expenses_maint = () => expenses_maint() * disc_factor()
-export const pv_expenses_acq = () => expenses_acq() * disc_factor()
-export const pv_commissions = () => commissions() * disc_factor()
-export const pv_net_cf = () => net_cf() * disc_factor()
-export const pv_pols_if = () => pols_if() * disc_factor() // BasicTerm_M, for pricing table calc reconciliation
+export const pv_claims = () => claims() * disc_factor();
+export const pv_premiums = () => premiums() * disc_factor();
+export const pv_expenses = () => expenses() * disc_factor();
+export const pv_expenses_maint = () => expenses_maint() * disc_factor();
+export const pv_expenses_acq = () => expenses_acq() * disc_factor();
+export const pv_commissions = () => commissions() * disc_factor();
+export const pv_net_cf = () => net_cf() * disc_factor();
+export const pv_pols_if = () => pols_if() * disc_factor(); // BasicTerm_M, for pricing table calc reconciliation
 
 // 2. pv_fut_x is sum of future present values >= t:
 export const pv_fut_claims = () => {
-  if (t() >= proj_len()) return 0
-  return (pv_fut_claims({ t_in: t() + 1 }) + pv_claims())
-}
+  if (t() >= proj_len()) return 0;
+  return pv_fut_claims({ t_in: t() + 1 }) + pv_claims();
+};
 
 export const pv_fut_premiums = () => {
-  if (t() >= proj_len()) return 0
-  return (pv_fut_premiums({ t_in: t() + 1 }) + pv_premiums())
-}
+  if (t() >= proj_len()) return 0;
+  return pv_fut_premiums({ t_in: t() + 1 }) + pv_premiums();
+};
 
 export const pv_fut_expenses = () => {
-  if (t() >= proj_len()) return 0
-  return (pv_fut_expenses({ t_in: t() + 1 }) + pv_expenses())
-}
+  if (t() >= proj_len()) return 0;
+  return pv_fut_expenses({ t_in: t() + 1 }) + pv_expenses();
+};
 
 export const pv_fut_expenses_maint = () => {
-  if (t() >= proj_len()) return 0
-  return (pv_fut_expenses_maint({ t_in: t() + 1 }) + pv_expenses_maint())
-}
+  if (t() >= proj_len()) return 0;
+  return pv_fut_expenses_maint({ t_in: t() + 1 }) + pv_expenses_maint();
+};
 
 export const pv_fut_expenses_acq = () => {
-  if (t() >= proj_len()) return 0
-  return (pv_fut_expenses_acq({ t_in: t() + 1 }) + pv_expenses_acq())
-}
+  if (t() >= proj_len()) return 0;
+  return pv_fut_expenses_acq({ t_in: t() + 1 }) + pv_expenses_acq();
+};
 
 export const pv_fut_commissions = () => {
-  if (t() >= proj_len()) return 0
-  return (pv_fut_commissions({ t_in: t() + 1 }) + pv_commissions())
-}
+  if (t() >= proj_len()) return 0;
+  return pv_fut_commissions({ t_in: t() + 1 }) + pv_commissions();
+};
 
 export const pv_fut_net_cf = () => {
-  if (t() >= proj_len()) return 0
-  return (pv_fut_net_cf({ t_in: t() + 1 }) + pv_net_cf())
-}
+  if (t() >= proj_len()) return 0;
+  return pv_fut_net_cf({ t_in: t() + 1 }) + pv_net_cf();
+};
 
 export const pv_fut_pols_if = () => {
-  if (t() >= proj_len()) return 0
-  return (pv_fut_pols_if({ t_in: t() + 1 }) + pv_pols_if())
-}
+  if (t() >= proj_len()) return 0;
+  return pv_fut_pols_if({ t_in: t() + 1 }) + pv_pols_if();
+};
 
 // not ported: check_pv_net_cf
-
-
--- src/cul/playground.cul.js
+++ src/cul/playground.cul.js
@@ -3,160 +3,170 @@
 // To make the playground model reducing term assurance product types
 // see comments surrounding `claim_pp` in basicterm.cul.js
 
-import { all_cul } from './basicterm.cul.js';
+import { all_cul } from "./basicterm.cul.js";
 
 // PARAMETERS FOR CONTROLS AND STRESSES
 
 // in basicterm.cul.js some things are hardcoded,
 // in the Playground we want to change them:
 
-export const commission_mths = () => commission_mths_in ?? 12
-export const commission_pc = () => commission_pc_in ?? 50
-export const expense_acq = () => expense_acq_in ?? 300
+export const commission_mths = () => commission_mths_in ?? 12;
+export const commission_pc = () => commission_pc_in ?? 50;
+export const expense_acq = () => expense_acq_in ?? 300;
 
 // there are other new inputs below that are also stressed
 
 // experience functionality for mortality and lapses is in basicterm.cul.js
 // see mort_rate_mth and lapse_rate_mth
 
-
-export const lapse_rate_factor = () => lapse_rate_factor_in ?? 1
+export const lapse_rate_factor = () => lapse_rate_factor_in ?? 1;
 
 // we use this optionally for pricing basis, see premium_rate_per_mille
-export const original_lapse_rates = () => original_lapse_rates_in ?? false
+export const original_lapse_rates = () => original_lapse_rates_in ?? false;
 
 export const lapse_rate = () => {
-  if (t() < stress_delay()) return original_lapse_rates() ? Math.max(0.1 - 0.02 * duration(), 0.02) : lapse_rate_();
-  else return lapse_rate_factor() * (original_lapse_rates() ? Math.max(0.1 - 0.02 * duration(), 0.02) : lapse_rate_());
-}
+  if (t() < stress_delay())
+    return original_lapse_rates()
+      ? Math.max(0.1 - 0.02 * duration(), 0.02)
+      : lapse_rate_();
+  else
+    return (
+      lapse_rate_factor() *
+      (original_lapse_rates()
+        ? Math.max(0.1 - 0.02 * duration(), 0.02)
+        : lapse_rate_())
+    );
+};
 
 export const inflation_rate_addition = () => inflation_rate_addition_in ?? 0;
 
 export const inflation_rate = () => {
-  if (t() < stress_delay()) return (inflation_rate_in ?? 0.02);
-  else return (inflation_rate_in ?? 0.02) + inflation_rate_addition()
-}
+  if (t() < stress_delay()) return inflation_rate_in ?? 0.02;
+  else return (inflation_rate_in ?? 0.02) + inflation_rate_addition();
+};
 
-export const maint_expenses_factor = () => maint_expenses_factor_in ?? 1
+export const maint_expenses_factor = () => maint_expenses_factor_in ?? 1;
 
 // incl. configurable expenses
 export const expense_maint = () => {
-  if (t() < stress_delay()) return (expense_maint_in ?? 60);
-  else return (expense_maint_in ?? 60) * maint_expenses_factor() // additives can go here
-}
+  if (t() < stress_delay()) return expense_maint_in ?? 60;
+  else return (expense_maint_in ?? 60) * maint_expenses_factor(); // additives can go here
+};
 
 // acquisition expenses not affected by stress
 
 // stressing mortality and lapse rates
 
-export const stress_delay = () => stress_delay_in ?? 0
+export const stress_delay = () => stress_delay_in ?? 0;
 
 export const mort_rate_factor = () => mort_rate_factor_in ?? 1;
 
-export const mort_rate_Y1_add_per_mille = () => mort_rate_Y1_add_per_mille_in ?? 0
+export const mort_rate_Y1_add_per_mille = () =>
+  mort_rate_Y1_add_per_mille_in ?? 0;
 
 export const mort_rate_addition = () => {
-  if (t() < (12 + stress_delay()))
-    return mort_rate_Y1_add_per_mille() / 1000
-  else return 0
-}
-
+  if (t() < 12 + stress_delay()) return mort_rate_Y1_add_per_mille() / 1000;
+  else return 0;
+};
 
 // PRICING
 
-export const loading_prem = () => loading_prem_in ?? 0.8
+export const loading_prem = () => loading_prem_in ?? 0.8;
 
-export const gender_neutral_pricing = () => gender_neutral_pricing_in ?? true
+export const gender_neutral_pricing = () => gender_neutral_pricing_in ?? true;
 
-export const update_pricing_lapse_rates = () => update_pricing_lapse_rates_in ?? false
+export const update_pricing_lapse_rates = () =>
+  update_pricing_lapse_rates_in ?? false;
 
 export const pricing_projection = () => pricing_projection_in ?? false;
 
 export const net_premium_pp = () => {
   if (pricing_projection())
-    return net_premium_pp_({ /* 1000e sum assured projection with pricing config and no stresses, and discounting always on */ sum_assured_in: 1000, original_lapse_rates_in: !update_pricing_lapse_rates(), discounting_on_in: true, sex_in: gender_neutral_pricing() ? 'F' : sex(), timing_in: 'BEF_DECR', stress_delay_in: 12 * 120, lapse_rate_factor_in: 1, mort_rate_factor_in: 1, mort_rate_Y1_add_per_mille_in: 0 });
-  else
-    return net_premium_pp_();
-}
+    return net_premium_pp_({
+      /* 1000e sum assured projection with pricing config and no stresses, and discounting always on */ sum_assured_in: 1000,
+      original_lapse_rates_in: !update_pricing_lapse_rates(),
+      discounting_on_in: true,
+      sex_in: gender_neutral_pricing() ? "F" : sex(),
+      timing_in: "BEF_DECR",
+      stress_delay_in: 12 * 120,
+      lapse_rate_factor_in: 1,
+      mort_rate_factor_in: 1,
+      mort_rate_Y1_add_per_mille_in: 0,
+    });
+  else return net_premium_pp_();
+};
 
 export const premium_rate_per_mille = () =>
-  (1 + loading_prem())
-  * net_premium_pp({pricing_projection_in: true});
+  (1 + loading_prem()) * net_premium_pp({ pricing_projection_in: true });
 
-
-export const pv_pols_if = () => pv_pols_if_() * premium_due() // used by net_premium_pp via pv_fut_pols_if
-
+export const pv_pols_if = () => pv_pols_if_() * premium_due(); // used by net_premium_pp via pv_fut_pols_if
 
 // DIFFERENT PREMIUM FREQUENCIES
 
-export const premium_payment_frequency = () => premium_payment_frequency_in ?? "M" // or "single"
+export const premium_payment_frequency = () =>
+  premium_payment_frequency_in ?? "M"; // or "single"
 
 export const premium_period = () => {
   switch (premium_payment_frequency()) {
     case "Y":
-      return 12
+      return 12;
     case "Q":
-      return 3
+      return 3;
     case "H":
-      return 6
+      return 6;
     default:
-      return 1
+      return 1;
   }
-}
+};
 
 export const premium_due = () => {
-  if (premium_payment_frequency() == "single")
-    return t() == -duration_mth_0()
-  if (duration() != duration({ t_in: t() - 1 })) return 1
-  if (duration_mth() % premium_period() == 0) return 1
-  else return 0
-}
+  if (premium_payment_frequency() == "single") return t() == -duration_mth_0();
+  if (duration() != duration({ t_in: t() - 1 })) return 1;
+  if (duration_mth() % premium_period() == 0) return 1;
+  else return 0;
+};
 
 // NOT annualized
 export const premium_pp = () => {
-  if (!premium_due()) return 0
-  return premium_pp_()
-}
+  if (!premium_due()) return 0;
+  return premium_pp_();
+};
 
 // MORTALITY RATE RECALC
 
-export const a = () => 0.0002
-export const b = () => 0.00025
-export const c = () => 2.2
-
+export const a = () => 0.0002;
+export const b = () => 0.00025;
+export const c = () => 2.2;
 
 // parameterised mortality formula from https://github.com/lifelib-dev/lifelib/blob/main/lifelib/libraries/basiclife/basic_term_sample.xlsx
 // Male mortality uplift added for Playground (basicterm does't have M/F rates) (but not necessarily used in premium projection: see `gender_neutral_pricing`)
-export const mort_rate_recalc = () => Math.min(1, Math.min(1, a() * Math.exp(b() * Math.pow(age(), c()))) * Math.pow(1.1, mort_rate_select_index()) * (sex() == 'M' ? 1.2 : 1))
+export const mort_rate_recalc = () =>
+  Math.min(
+    1,
+    Math.min(1, a() * Math.exp(b() * Math.pow(age(), c()))) *
+      Math.pow(1.1, mort_rate_select_index()) *
+      (sex() == "M" ? 1.2 : 1),
+  );
 
 export const mort_rate = () => {
-  if (t() < stress_delay()) return mort_rate_recalc()
-  else return mort_rate_recalc() * mort_rate_factor() + mort_rate_addition() // note: added after factor on overall assumption
-}
-
-
-
-
+  if (t() < stress_delay()) return mort_rate_recalc();
+  else return mort_rate_recalc() * mort_rate_factor() + mort_rate_addition(); // note: added after factor on overall assumption
+};
 
 // DISCOUNTING CONTROL
 
-export const discounting_on = () => discounting_on_in ?? true
+export const discounting_on = () => discounting_on_in ?? true;
 
 export const disc_rate_ann = () => {
-  if (!discounting_on()) return 0
-  else return disc_rate_ann_()
-}
-
-
+  if (!discounting_on()) return 0;
+  else return disc_rate_ann_();
+};
 
 // MISCEL
 
-export const premium_pp_0 = () => premium_pp({ t_in: -duration_mth_0() }) // to show the premium
-export const status = () => duration_mth_0() == 0 ? 'New Business' : 'In Force'
-
-
-
-
+export const premium_pp_0 = () => premium_pp({ t_in: -duration_mth_0() }); // to show the premium
+export const status = () =>
+  duration_mth_0() == 0 ? "New Business" : "In Force";
 
 // PLACEHOLDER CASHFLOWS FOR PLAYGROUND CUSTOMIZATION:
 
@@ -164,15 +174,13 @@ export const status = () => duration_mth_0() == 0 ? 'New Business' : 'In Force'
 // careful: manipulation in net_cf not captured in Playground outputs!
 export const net_cf = () => net_cf_() + placeholder() + placeholder2();
 
-export const placeholder = () => 0 // placeholder1 will be visualized green
-export const placeholder2 = () => 0 // placeholder2 will be visualized purpley
-
+export const placeholder = () => 0; // placeholder1 will be visualized green
+export const placeholder2 = () => 0; // placeholder2 will be visualized purpley
 
 // we must have PVs to visualize. Even if discounting is off, this is what the visualization uses
 // (when discounting)
-export const pv_placeholder = () => disc_factor() * placeholder()
-export const pv_placeholder2 = () => disc_factor() * placeholder2()
-
+export const pv_placeholder = () => disc_factor() * placeholder();
+export const pv_placeholder2 = () => disc_factor() * placeholder2();
 
 // policy value is present value of future cashflows, at t_in=0
-export const policy_value = () => pv_fut_net_cf({ t_in: 0 })
+export const policy_value = () => pv_fut_net_cf({ t_in: 0 });
